1b9afd1d192af5ddbb266771726e0d9d659bef48
Diego Popritkin <dpopritkin@intranet.certant.com>
2021-04-28 11:30:50 -0300

    cambios similares a moa

--- src/main/resources/initializeDB.sql
+++ src/main/resources/initializeDB.sql
@@ -1,3 +1,3 @@
-insert into fruit (name) values ('Cherry');
-insert into fruit (name) values ('Apple');
-insert into fruit (name) values ('Banana');
+insert into credential (username,password) values ('Cherry','Cherry');
+insert into credential (username,password) values ('Apple','Apple');
+insert into credential (username,password) values ('Banana','Banana');
